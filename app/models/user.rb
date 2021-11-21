75c91b1b31c7eb79ee4385f4ea8ed9c9170f1b74
David Florez <dflorez6@gmail.com>
2021-11-20 19:40:53 -0500

    v 1.0.0.1
    # KANUT | Web App #
    
    ## Front-End ##
    * Devise Views - basic UI
    
    ## Back-End ##
    * User Scaffold
    * Devise for User
    * Routes - Resources: User & Devise: User
    * Migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  #====================
+  # Validations
+  #====================
 
 
+  #====================
+  # Associations
+  #====================
+
+  #====================
+  # Scopes
+  #====================
+  default_scope { order(last_name: :asc) }
+
+  #====================
+  # Methods
+  #====================
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  resources :users
+
+
   # =========================
   # Root
   # =========================
   root to: 'static_pages#index'
 
+  # =========================
+  # Devise
+  # =========================
+  devise_for :users
+  devise_scope :user do
+    get '/login', to: 'devise/sessions#new'
+    # match '/auth/users/sign_up.user', to: 'registrations#create', via: :post
+  end
+
+  # =========================
+  # Resources
+  # =========================
+  resources :users
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
